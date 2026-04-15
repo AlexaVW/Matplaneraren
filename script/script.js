d6dd55ca2417efa0a71b976638af35eb4fecfc52
Alexandra Westas <alexandra.westas1999@hotmail.com>
2026-04-15 20:08:59 +0200

    Refactor

--- script/script.js
+++ script/script.js
@@ -1,40 +1,38 @@
-
 let selectedIngredient = null; 
 let startPos = 0;
 let getAmount = 100;
 let urlIngredient = "https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livsmedel?offset=" + startPos + "&limit=" + getAmount + "&sprak=1";
 
-const ingredientsContainer = document.getElementById("ingredientsContainer");
+const ingredientsContainer = document.getElementById("ingredientsContainer"); 
 const searchText = document.getElementById("search-text"); 
 const ingredientOptions = document.getElementById("ingredientOptions");
 
 searchText.addEventListener("input", getIngredientOptions); 
 document.getElementById("addButton").addEventListener("click", addIngredient); 
 
-let ingredients = [];
-getIngredients();
-let selectedIngredients = [];
+let ingredients = []; 
+getIngredients(); 
+let selectedIngredients = []; 
 
-
+// Get data from API----------------------------------------------------------------------
 function getIngredients() {
     fetch(urlIngredient)
         .then(
             function (response) {
-                return response.json();
+                return response.json(); 
             }
         )
         .then(
             function (data) {
                 console.log(data);
-                console.log(data.livsmedel);
-                data.livsmedel.map(ingredientApi => {
-                    ingredients.push({
+                console.log(data.livsmedel); 
+                data.livsmedel.map(ingredientApi => { 
+                    ingredients.push({ 
                         ingredientId: ingredientApi.nummer,
                         ingredientName: ingredientApi.namn
                     });
                 });
-                console.log("ingredientsarray: ", ingredients);
-                console.log("number of ingredients: ", ingredients.length);
+                console.log("ingredientsarray: ", ingredients); 
             }
         )
         .catch(
@@ -44,8 +42,9 @@ function getIngredients() {
         );
 }
 
+
 function getNutritions(ingredient) { 
-    let urlNutrition = "https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livsmedel/" + ingredient.ingredientId + "/naringsvarden?sprak=1";
+    let urlNutrition = "https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livsmedel/" + ingredient.ingredientId + "/naringsvarden?sprak=1"; 
     fetch(urlNutrition)
         .then(
             function (response) {
@@ -55,7 +54,7 @@ function getNutritions(ingredient) {
         .then(
             function (data) {
                 console.log(data);
-                createCard(ingredient, data); 
+                createCard(ingredient, data); // The card is created after the nutritions are fetched
             }
         )
         .catch(
@@ -65,6 +64,8 @@ function getNutritions(ingredient) {
         );
 }
 
+// Search---------------------------------------------------------------
+// Filtering the list with ingredients and showing the options in a dropdown
 function getIngredientOptions() {
     ingredientOptions.innerHTML = ""; 
 
@@ -75,43 +76,34 @@ function getIngredientOptions() {
             let option = document.createElement("p"); 
             option.textContent = ingredient.ingredientName; 
 
+            // When user clicks on an option from the dropdown
             option.onclick = function () { 
                 searchText.value = ingredient.ingredientName; 
-                selectedIngredient = ingredient; 
+                selectedIngredient = ingredient; // Storing the selected ingredient
                 ingredientOptions.innerHTML = ""; 
-                console.log("Ingrediens som läggs till: ", selectedIngredient); 
             }
+
+            // Adding the option to the dropdown
             ingredientOptions.appendChild(option); 
         }
     });
 }
 
-function addIngredient() {
-    if (selectedIngredient == null) {
+
+// Add ingredient-----------------------------------------------------------
+function addIngredient() { 
+    if (selectedIngredient == null) { 
         return;
     }
 
-    getNutritions(selectedIngredient) 
+    // Getting the nutritions from the selected ingredient, then creating the card
+    getNutritions(selectedIngredient); 
 }
 
+// Create card for the ingredient--------------------------------------------
 function createCard(ingredient, dataNutrition) {
 
-    //dataNutrition is the array that have all the nutritions for the selected ingredient 
-    let foundCarbs = dataNutrition.find(item => item.namn == "Kolhydrater, tillgängliga"); 
-    let foundProtein = dataNutrition.find(item => item.namn == "Protein");
-    let foundFat = dataNutrition.find(item => item.namn == "Fett, totalt");
-    let foundKcal = dataNutrition.find(item => item.namn == "Energi (kcal)");
-
-    console.log(foundCarbs);
-    console.log(foundProtein);
-    console.log(foundFat);
-    console.log(foundKcal);
-
-    let nutrition = {carbs: foundCarbs.varde,
-        protein: foundProtein.varde,
-        fat: foundFat.varde,
-        calories: foundKcal.varde
-    };
+    let nutrition = getSelectedNutrition(dataNutrition);
 
     let card = document.createElement("div");
     card.setAttribute("class", "card");
@@ -131,12 +123,12 @@ function createCard(ingredient, dataNutrition) {
     let calories = document.createElement("p");
     calories.textContent = "Kalorier: " + nutrition.calories + " kcal";
 
-    //--------------------------------------------------
+    //-----------------------------------------------------------------
     let slider = createSlider();
 
     let grams = document.createElement("p");
 
-    //Every card gets its own slider with its own span for grams
+    // Every card gets its own slider with its own span for grams
     let startValueGrams = document.createElement("span"); 
     startValueGrams.textContent = "100";
 
@@ -153,18 +145,18 @@ function createCard(ingredient, dataNutrition) {
         grams: 100 //Starts at 100
     };
 
-    //Adding the values to an array to be able to calculate on it
+    // To be able to calculate on it
     selectedIngredients.push(ingredientValues); 
 
-    //What happens with the slider when changing value.
-    //The function saves referenses to slider and startValueGrams that is created when addIngredients runs
+    // What happens with the slider when changing value.
+    // The function saves referenses to slider and startValueGrams that is created when addIngredients runs
     slider.oninput = function () {
         let valueGrams = slider.value; 
         startValueGrams.textContent = valueGrams; 
 
         ingredientValues.grams = valueGrams; 
 
-        //Calculating for 1g. Multiplying it with the new value.
+        // Calculating for 1g. Multiplying it with the new value.
         let changedCarbsValue = Math.round((nutrition.carbs / 100) * valueGrams); 
         let changedProteinValue = Math.round((nutrition.protein / 100) * valueGrams);
         let changedFatValue = Math.round((nutrition.fat / 100) * valueGrams);
@@ -178,19 +170,7 @@ function createCard(ingredient, dataNutrition) {
         calculateTotalNutrition();
     }
 
-    let deleteButton = document.createElement("input");
-    deleteButton.setAttribute("type", "button");
-    deleteButton.setAttribute("value", "Ta bort")
-    deleteButton.onclick = function () { //The function knows which card it's connected to. Works without id because I use the card which works directly with the object in the memory
-        card.remove(); 
-
-        //To delete the data
-        let index = selectedIngredients.indexOf(ingredientValues);
-        selectedIngredients.splice(index, 1); 
-        
-        //Calculating again after everything is removed
-        calculateTotalNutrition();
-    }
+    let deleteButton = createDeleteButton(card, ingredientValues);
 
     card.appendChild(title);
     card.appendChild(carbs);
@@ -206,6 +186,23 @@ function createCard(ingredient, dataNutrition) {
     calculateTotalNutrition();
 }
 
+// createCard functions-----------------------------------------------------------------
+function getSelectedNutrition(dataNutrition){
+    // dataNutrition is the array that have all the nutritions for the selected ingredient 
+    let foundCarbs = dataNutrition.find(item => item.namn == "Kolhydrater, tillgängliga"); 
+    let foundProtein = dataNutrition.find(item => item.namn == "Protein");
+    let foundFat = dataNutrition.find(item => item.namn == "Fett, totalt");
+    let foundKcal = dataNutrition.find(item => item.namn == "Energi (kcal)");
+
+    let nutrition = {carbs: foundCarbs.varde,
+        protein: foundProtein.varde,
+        fat: foundFat.varde,
+        calories: foundKcal.varde
+    };
+    return nutrition;
+}
+
+
 function createSlider() {
     let slider = document.createElement("input");
     slider.setAttribute("type", "range");
@@ -215,6 +212,26 @@ function createSlider() {
     slider.setAttribute("class", "slider");
     return slider;
 }
+
+
+function createDeleteButton(card, ingredientValues){
+    let deleteButton = document.createElement("input");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.setAttribute("value", "Ta bort");
+    deleteButton.setAttribute("class", "buttons");
+    deleteButton.onclick = function () { // The function knows which card it's connected to. Works without id because I use the card which works directly with the object in the memory
+        card.remove(); 
+
+        // To delete the data
+        let index = selectedIngredients.indexOf(ingredientValues);
+        selectedIngredients.splice(index, 1); 
+        
+        // Calculating again after everything is removed
+        calculateTotalNutrition();
+    }
+    return deleteButton;
+}
+
 
 function calculateTotalNutrition(){
     let totalCarbs = 0;
